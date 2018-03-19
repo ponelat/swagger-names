a60730bd7dea82a506686ee5f312247c2dc73d56
Josh Ponelat <jponelat@gmail.com>
2018-03-19 12:38:43 +0200

    update dockerfile and index.js

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,16 @@
 FROM mhart/alpine-node:8.7.0
 
+RUN apk add --no-cache tini
+# Tini is now available at /sbin/tini
+ENTRYPOINT ["/sbin/tini", "--", "node", "./index.js"]
+
 WORKDIR /app
 COPY ./package.json /app/package.json
 
 RUN npm install
 
-ENV SERVER_MODE
+# Set to 1, to start a server instead of just a cli
+ENV SERVER_MODE=
+EXPOSE 3000
 
 COPY . /app/
-
-ENTRYPOINT ["node", "./index.js"]
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ if(serverMode) {
     console.log('No input string provided!')
     console.log('Usage: docker run --rm ponelat/swagger-names <input-string>')
     console.log('Or')
-    console.log('Usage: docker run --rm -e "SERVER_MODE=1" ponelat/swagger-names')
+    console.log('Usage: docker run --rm -e "SERVER_MODE=1" -p 80:3000 ponelat/swagger-names')
     process.exit(1)
   }
   console.log(getName(inputStr))
